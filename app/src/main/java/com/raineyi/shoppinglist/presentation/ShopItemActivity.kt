469d9ee5868abc30f04e1f373e3abbeeff1b1a6a
Rainey <rainey888@mail.ru>
2023-12-30 18:17:36 +0700

    - Add layout for ShopItemActivity

---
+++ app/src/main/java/com/raineyi/shoppinglist/presentation/ShopItemActivity.kt
@@ -0,0 +1,12 @@
+package com.raineyi.shoppinglist.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.raineyi.shoppinglist.R
+
+class ShopItemActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_shop_item)
+    }
+}
